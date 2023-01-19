f0a91d20cbf21361369cbfe5e4cb1a274e4f486a
gsnorberto <sn_gabriel@outlook.com>
2023-01-19 13:53:32 -0300

    style: página home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,28 @@
-import { HomeArea } from "./styles"
+import { HomeArea, Container, ContentArea, Header, Title, Link, Button, Text, ButtonArea, Buttons } from "./styles"
+import { IoAddCircleOutline, IoRemoveCircleOutline, IoExitOutline } from "react-icons/io5";
 
 export default () => {
     return (
         <HomeArea>
-            
+            <Container>
+                <Header>
+                    <Title>Olá, Fulano</Title>
+                    <Link><IoExitOutline className="io-exit" /></Link>
+                </Header>
+                <ContentArea>
+                    Não há registros de entrada ou saída
+                </ContentArea>
+                <Buttons>
+                    <ButtonArea>
+                        <Button><IoAddCircleOutline className="icon-circle" /></Button>
+                        <Text>Nova entrada</Text>
+                    </ButtonArea>
+                    <ButtonArea>
+                        <Button><IoRemoveCircleOutline className="icon-circle" /></Button>
+                        <Text>Nova saída</Text>
+                    </ButtonArea>
+                </Buttons>
+            </Container>
         </HomeArea>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,71 @@
 import styled from "styled-components";
 
 export const HomeArea = styled.div`
+    font-family: 'Raleway', sans-serif;
+    background-color: #8C11BE;
+    min-height: 100vh;
+`
+export const Container = styled.div`
+    max-width: 400px;
+    height: 100vh;
+    padding: 10px 15px;
+    margin: 0 auto;
+`
+export const Header = styled.div`
+    padding-top: 20px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
+    .io-exit{
+        width: 25px;
+        height: 25px;
+        color: #FFFFFF;
+    }
+`
+export const ContentArea = styled.div`
+    margin-top: 22px;
+    margin-bottom: 12px;
+    width: 100%;
+    min-height: 440px;
+    background-color: #FFFFFF;
+    border-radius: 5px;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #868686;
+`
+export const Title = styled.div`
+    font-weight: 700;
+    color: #FFFFFF;
+    font-size: 26px;
+`
+export const Link = styled.a`
+    
+`
+export const Button = styled.a`
+    margin-bottom: 31px;
+    .icon-circle{
+        width: 22px;
+        height: 22px;  
+    }
+`
+export const ButtonArea = styled.div`
+    color: #FFFFFF;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    background-color: #A328D6;
+    width: 49%;
+    border-radius: 5px;
+    //align-items: flex-start;
+`
+export const Buttons = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+export const Text = styled.div`
+    max-width: 40px;
 `
